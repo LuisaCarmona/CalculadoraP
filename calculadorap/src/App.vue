0acd425bc1bf4a9b3eedff3eae654097b68a3886
Luisa Carmona <cp.carmonaclf@gmail.com>
2021-09-18 11:41:43 -0500

    firts commit

---
+++ calculadorap/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<v-app>
+  <NavBar></NavBar>
+  <v-main style="margin-top:80px">
+    <v-container fluid>
+    <router-view></router-view>
+    </v-container>
+  </v-main>
+  </v-app>  
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+  
+};
+</script>
---
+++ calculadorap/src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-app-bar
+      absolute
+      color="blue"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>CALCULADORA PECUARIA</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/login" style="text-decoration: none"><v-btn text large class="ma-2">
+        Login
+      </v-btn></router-link>
+
+      <router-link to="/sign-in" style="text-decoration: none"><v-btn text large class="ma-2">
+        Registro
+      </v-btn></router-link>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
